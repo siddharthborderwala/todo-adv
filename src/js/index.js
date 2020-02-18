4646444892b89b5115be22bbc3e117f4e998c33b
siddharthborderwala <siddharthborderwala@users.noreply.github.com>
2020-02-18 14:02:14 +0530

    over

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
-import '../sass/main.scss';
+import "../sass/main.scss";
+
+const todoBaseMarkup = `
+	<div class="todo">
+		<input class="todo__check" type="checkbox">
+		<span class="todo__cross">&times;</span>
+		<h2 class="todo__title">
+			%todo-title%
+		</h2>
+	</div>
+`;
+
+document.querySelector('.header__date').innerHTML = new Date().toLocaleDateString('en-UK', {
+	month: 'long',
+	day: 'numeric'
+}).concat(`<sup>${new Date().getDay.toString().endsWith('1') ? 'nd' : new Date().getDay.toString().endsWith('2') ? 'nd' : new Date().getDay.toString().endsWith('3') ? 'rd' : 'th'}<sup>`);
+
+document.querySelector('.field__form').addEventListener('submit' , ev => {
+	ev.preventDefault();
+	const newTodoTitle = document.querySelector('.field__input').value;
+	const newTodoMarkup = todoBaseMarkup.replace('%todo-title%', newTodoTitle[0].toUpperCase().concat(newTodoTitle.slice(1)));
+	document.querySelector('.ground').insertAdjacentHTML('beforeend', newTodoMarkup);
+	document.querySelector('.field__input').value = "";
+	Array.from(document.querySelectorAll('.todo__cross')).forEach(el => {
+		el.addEventListener('click', () => {
+			el.parentElement.remove();
+		});
+	});
+});
